35bbaf013344c52641fa75e6916b1a8fc5494078
challenger1736 <challenger1736@gmail.com>
2023-12-26 19:55:53 +0900

    20231226T1953 commit

--- site/js/cAccount.js
+++ site/js/cAccount.js
@@ -6,10 +6,28 @@ function _signup2(){
     const ezenPw = document.querySelector('#ezenPw').value;
     const ezenPwCheck = document.querySelector('#ezenPwcheck').value;
     console.log(ezenId); console.log(ezenPw); console.log(ezenPwCheck);
+    const ezenName = document.querySelector('#ezenName').value;
+    let ezenGrade = document.querySelector('#ezenGrade').value;
     if(ezenPw !== ezenPwCheck){ alert('비밀번호/비밀번호 확인이 다릅니다!'); return;}
 
-    
+    if(ezenId, ezenPw, ezenPwCheck, ezenName == ''){alert('빈 칸을 입력하세요.'); return;}
 
-    const 
+
+    if(ezenGrade == '학생'){ ezenGrade = 1 ;}
+    if(ezenGrade == '강사'){ ezenGrade = 4 ;}
+    console.log(ezenGrade)
+
+    const identify = { ezenId : ezenId ,
+        ezenPw : ezenPw,
+        ezenName : ezenName,
+        ezenGrade : ezenGrade
+    }
+
+    let identifyArray = JSON.parse(localStorage.getItem('identifyArray'))
+    if (identifyArray==null){ identifyArray=[]}
+    identifyArray.push(identify)
+    localStorage.setItem('identifyArray' , JSON.stringify(identifyArray) )
+    alert('회원가입 완료.')
+    location.href = '/site/main.html' // 말고 로그인 페이지로 가게 하기
 
 }
---
+++ site/js/login.js
@@ -0,0 +1,22 @@
+console.log('로그인 js 실행')
+
+function login(){
+    console.log('로그인 펑션 실행')
+
+    const loginId = document.querySelector('#loginId').value;
+    const loginPw = document.querySelector('#loginPw').value;
+
+    const identifyArray = JSON.parse( localStorage.getItem('identifyArray')); 
+    console.log(identifyArray)
+    const login = { loginId: loginId, loginPw: loginPw};
+
+    for(let i =0 ; i<identifyArray.length ; i ++){
+        console.log('for문 실행')
+        if(identifyArray[i].ezenId == login.loginId && identifyArray[i].ezenPw ==login.loginPw ){
+            alert('로그인 성공');
+            location.href="/site/main.html"
+            return;
+        }
+    }
+    alert('동일한 회원정보가 없습니다.'); return;
+}
