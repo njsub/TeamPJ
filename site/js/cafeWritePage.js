5232db8df386b81a41a59350a38222c2ed48ff72
gusdl1992 <tlgusdl92@gmali.com>
2023-12-28 23:28:32 +0900

    20231228 23:28

--- site/js/cafeWritePage.js
+++ site/js/cafeWritePage.js
@@ -45,3 +45,59 @@ function cafeWritePage(){/* 글쓰기 페이지로 가는 함수 */
 
 
 //까지가 로그인된 카페 글 쓰기 화면 함수
+
+
+//박시현 작업 끝 2023-12-28 23:28
+// 내일 마무리로 선택가능한 게시판만 보여주게 하면 될것 같다...
+
+function 게시글등록(){
+    // 회원정보 가져오기
+    let identifyArray = JSON.parse(localStorage.getItem('identifyArray'))
+    // 현재 로그인중인 회원 가져오기
+    let ezenLogin = JSON.parse(localStorage.getItem('ezenLogin'))   
+    console.log(ezenLogin.loginId)
+    // 글쓰기 값 가져오기
+    const cfTitle = document.querySelector('#cftitle').value
+    const cfSelect = document.querySelector('#contentSelect').value
+    const cfContent = document.querySelector('textarea').value;
+    // 현재 로그인중인 ID와 회원배열에있는 값 비교하여 ezenMNo 가져오기
+    let ezenMNo = 0;
+    for(let i = 0; i < identifyArray.length; i++){
+        if(ezenLogin.loginId == identifyArray[i].ezenId){
+            ezenMNo = identifyArray[i].ezenMNo
+        }
+    }
+
+      // 유효성 검사
+      if(cfTitle.length < 1){ alert('제목을 입력해주세요.'); return};
+      if(cfSelect == '선택'){ alert('게시판을 선택하세요.'); return};
+      if(cfContent.length < 1){ alert('내용을 입력해주세요.'); return};
+
+       //  localStorage 호출하여 글쓸 공간(배열)이 없으면 생성 
+    let cafeWriteList = JSON.parse( localStorage.getItem('cafeWriteList') ); console.log(cafeWriteList)
+    if( cafeWriteList == null){ cafeWriteList = [] }; // 만약에 localStorage 아무것도 없으면 배열 선언
+    
+    const cafeWrite = {
+        // [!] 게시물번호를 자동으로 순차적으로 배정.
+        cfno : cafeWriteList.length >=1 ? cafeWriteList[cafeWriteList.length-1].cfno +1 : 1 , // 글번호
+        ezenMNo :  ezenMNo ,    // 글쓸때 회원의 고유 회원번호
+        cfTitle : cfTitle ,  // 글쓰기 제목
+        cfsel : cfSelect , // 게시판 선택
+        cfdate : new Date()  ,   // 날짜시간 저장
+        cfContent : cfContent.replaceAll('\n' , '<br/>') ,      // 문자열.replaceAll('교체할문자' , '새로운문자')
+    }
+
+    // 글 저장 배열에 글 객체 저장
+    cafeWriteList.push(cafeWrite);
+    // localStorage 에 저장
+    localStorage.setItem('cafeWriteList' , JSON.stringify(cafeWriteList) );
+    // 등록 성공 메세지
+    alert('글등록 성공!')
+    // 게시판 기본값 초기화
+    document.querySelector('#cftitle').value = ''
+    document.querySelector('#contentSelect').value = "선택"
+    document.querySelector('textarea').value = ''
+
+
+}
+
