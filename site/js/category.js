3fa815ced41f32d3b4bc1a3dc037cd08fb13730a
geownk95 <geonwk95@naver.com>
2023-12-28 18:02:40 +0900

    20231228 dd

--- site/js/category.js
+++ site/js/category.js
@@ -1,30 +1,33 @@
 
-const 카테고리목록 = []
 
 function 카테고리등록(){
-
+    let 카테고리목록 = JSON.parse(localStorage.getItem('카테고리목록'))
+    if (카테고리목록==null){ 카테고리목록=[]}
     // 1. 어디에
 
-    const cnoBox = document.querySelector('#cnoBox').value;
     const cateBox = document.querySelector('#cateBox').value;
     const gradeBox = document.querySelector('#gradeBox').value;
     const gisuBox = document.querySelector('#gisuBox').value;
 
-    const categoryArray = {cno : cnoBox, 카테고리명 : cateBox, 권한 : gradeBox, 기수 : gisuBox}
+    const categoryArray = { 카테고리명 : cateBox, 권한 : gradeBox, 기수 : gisuBox, cno : 카테고리목록.length ==0 ? 1 : Number(카테고리목록[카테고리목록.length-1].cno)+1}
     카테고리목록.push(categoryArray); console.log(categoryArray)
 
    
     // 초기화
-    document.querySelector('#cnoBox').value ='';
+    
     document.querySelector('#cateBox').value = '';
     document.querySelector('#gradeBox').value = '';
     document.querySelector('#gisuBox').value = '';
 
     // 3. 출력
+    localStorage.setItem('카테고리목록' , JSON.stringify(카테고리목록) )
     카테고리목록출력()
+    
+
 }
 
 function 카테고리목록출력(){
+    let 카테고리목록 = JSON.parse(localStorage.getItem('카테고리목록'))
     // 1. 어디에
     const categoryList = document.querySelector('#categoryList')
 
--- site/js/member.js
+++ site/js/member.js
@@ -1,13 +1,13 @@
 console.log('')
 
-
-
+document.addEventListener('DOMContentLoaded' , function(){
 // 회원 저장 객체배열 불러오기
-let identifyArray = JSON.parse(localStorage.getItem('identifyArray'))
-console.log(identifyArray)
+
 
 회원목록출력()
+})
 function 삭제버튼(index){  
+    let identifyArray = JSON.parse(localStorage.getItem('identifyArray'))
     if(!confirm('해당 회원을 삭제하시겠습니까?')){
         
         alert('취소되었습니다.');
@@ -15,6 +15,7 @@ function 삭제버튼(index){
     else{
          //삭제처리
          identifyArray.splice(index, 1);
+         localStorage.setItem( 'identifyArray' ,JSON.stringify(identifyArray))
          //새로고침
         alert('삭제되었습니다.');
         회원목록출력();
@@ -25,15 +26,18 @@ function 삭제버튼(index){
 function 회원목록출력(){
     // 1. 어디에
     const memberList = document.querySelector('#memberList')
+    let identifyArray = JSON.parse(localStorage.getItem('identifyArray'))
+    console.log(identifyArray)
     // 2. 무엇을
     let html = ''
-    for(let i= 0; i<identifyArray.length; i++){
+    for(let i= 1; i<identifyArray.length; i++){
         html += `
         <tbody id="memberList">
         <td>1</td>
         <td>${identifyArray[i].ezenId}</td>
         <td>${identifyArray[i].ezenName}</td>
-        <td>${identifyArray[i].generation}</td>
+        <td>${identifyArray[i].ezenGrade}</td>
+        <td>${identifyArray[i].ezenGrade}</td>
         <td><button onclick="삭제버튼(${i})" value="삭제">삭제</button></td>
         </tbody>
         `
