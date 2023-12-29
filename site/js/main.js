5bd89209abf6b81401b35acd13ef60c3468739ca
challenger1736 <challenger1736@gmail.com>
2023-12-29 11:54:57 +0900

    20231229t 승호 커밋

--- site/js/main.js
+++ site/js/main.js
@@ -20,27 +20,31 @@ function 카테고리생성(){
     let 카테고리목록 = JSON.parse(localStorage.getItem('카테고리목록'))
     // 1. 어디에
     const lnbMenu = document.querySelector('#lnbMenu')
-
+    
+    
     // 2. 무엇을
-    let html = ''
+    let html = '';
+   
     for(let i= 0; i<카테고리목록.length; i++){
         html += `
-        <div  class="topLine">
-            <a onclick="접근권한(${카테고리목록[i].기수})" href="/site/html/member.html">${카테고리목록[i].카테고리명}</a>
+        <div class="mainMenuS" onclick="카테고리실행(${i})">
+            <a >${카테고리목록[i].카테고리명}</a>
         </div>
         `;
+        /* 메인 이너R에 넣을 html 정하기 */
+      
     }
     // 3. 출력
+    
     lnbMenu.innerHTML = html; 
 }
 
-// function 접근권한(){
 
-//     // 1. 
-//     if (카테고리목록[i].기수 == 카테고리)
-// }
-// if 게시판 접근 권한 = 카테고리목록[i].권한 == 카테고리생성.권한  접근가능
-// else 접근불가
+let 기수확인 = '';
+
+
+
+
 
 
 
@@ -102,6 +106,50 @@ function afterlogin(){
 
 })
 
+function 카테고리실행(i){
+    console.log('카테고리 눌렀을 때 실행')
+    // 일단 권한 확인
+    let 카테고리목록 = JSON.parse(localStorage.getItem('카테고리목록'))
+    let ezenLogin = JSON.parse(localStorage.getItem('ezenLogin'))
+    let identifyArray = JSON.parse(localStorage.getItem('identifyArray'))
+    const mainInnerR = document.querySelector('#mainInnerR')
+
+    let html = '';
+        for(let i =0 ; i<identifyArray.length; i++){if(identifyArray[i].ezenId==ezenLogin.loginId){
+           
+            기수확인 = identifyArray[i].generation //지금 로그인한 아이디의 기수
+            console.log(기수확인)
+             break; // break가 가장 가까운 반복문 종료
+        }}
+
+        console.log(카테고리목록[i].기수)
+        if(ezenLogin.loginId == '관리자'){ /* 관리자면 실행하게 */
+            
+
+        html += `<h2>${카테고리목록[i].카테고리명}</h2>
+        <div style="border-bottom: black 1px solid; height: 10px"> </div>
+        `
+    
+        mainInnerR.innerHTML = html
+
+            
+            return; }
+
+
+
+
+
+        else if(카테고리목록[i].기수!=기수확인 ){ alert('본인이 해당하는 기수가 아닙니다.');
+        location.href="/site/main.html";
+    }
+
+    
+    //확인 완료
+
+
+        }
+    // 1. 
+  
 
 
 /* 펑션안에 생으로 펑션 넣기는 안되나봄 */
@@ -134,7 +182,8 @@ function admin(){/* 관리자 추가 */
     ezenPw : '1234',
     ezenName : '카피조우',
     ezenGrade : 5,
-    ezenMNo: 1
+    ezenMNo: 1,
+    generation : '관리자'
 
 }
 
